fb29c1d12606f4ae5d78c28f8311e0a7d63eede7
ozelotjp <contact@ozelot.app>
2020-07-16 22:57:34 +0900

    chore: 依存パッケージをアップデート

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,24 @@
 // import colors from 'vuetify/es5/util/colors'
 
 export default {
-  mode: 'spa',
   srcDir: 'src',
   /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'spa',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
+  /*
    ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
     titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -20,25 +30,27 @@ export default {
     ]
   },
   /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
    ** Global CSS
    */
   css: [],
   /*
    ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
    */
   plugins: ['@/plugins/composition-api', '@/plugins/firebase'],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     '@nuxt/typescript-build',
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/vuetify',
   ],
   /*
    ** Nuxt.js modules
@@ -76,13 +88,9 @@ export default {
   },
   /*
    ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    // extend(config, ctx) { }
-  },
+  build: {},
   typescript: {
     typeCheck: {
       eslint: true
