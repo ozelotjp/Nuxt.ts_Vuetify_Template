32c253ae7b6f4a6884bc90b3d52252c5aca572ef
ozelotjp <contact@ozelot.app>
2020-07-19 01:06:53 +0900

    feat: indexの内容を整理

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,39 +1,19 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col>
-        <v-card>
-          <v-card-title>
-            Hello World
-          </v-card-title>
-          <v-card-text>
-            <p>Now Time: {{ now() }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <p>$vuetify.breakpoint.name: {{ screen }}</p>
   </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'nuxt-composition-api'
+import { defineComponent, computed } from 'nuxt-composition-api'
 
 export default defineComponent({
   middleware: 'authenticated',
-  setup() {
-    const now = () => {
-      const date = new Date()
-      const y = date.getFullYear()
-      const m = ('0' + (date.getMonth() + 1)).slice(-2)
-      const d = ('0' + date.getDate()).slice(-2)
-      const h = ('0' + date.getHours()).slice(-2)
-      const i = ('0' + date.getMinutes()).slice(-2)
-      const s = ('0' + date.getSeconds()).slice(-2)
-      return `${y}-${m}-${d} ${h}:${i}:${s}`
-    }
+  setup(_, { root: { $vuetify } }) {
+    const screen = computed(() => $vuetify.breakpoint.name)
 
     return {
-      now,
+      screen,
     }
   },
 })
