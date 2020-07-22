5e38d47d524ece1f7a2c37ff611643f36e2aca14
ozelotjp <contact@ozelot.app>
2020-07-22 09:38:06 +0900

    feat: 右側のドロワーの中身を仮実装

--- src/components/layout/LayoutRightSide.vue
+++ src/components/layout/LayoutRightSide.vue
@@ -1,21 +1,71 @@
 <template>
   <v-navigation-drawer v-model="drawer" absolute clipped right temporary>
+    <v-list dense nav>
+      <v-list-item-group>
+        <v-list-item two-line disabled>
+          <v-list-item-avatar>
+            <v-img :src="account.photoUrl" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="black--text">
+              {{ account.displayName }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ account.email }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+      <v-list-item-group color="primary">
+        <v-list-item to="/auth/logout">
+          <v-list-item-icon>
+            <v-icon>
+              mdi-logout-variant
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            ログアウト
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'nuxt-composition-api'
+import {
+  defineComponent,
+  ref,
+  reactive,
+  onBeforeMount,
+} from 'nuxt-composition-api'
 
 export default defineComponent({
-  setup() {
+  setup(_, { root: { $firebase } }) {
     const drawer = ref(false)
+    const account = reactive({
+      displayName: '',
+      email: '',
+      photoUrl: '',
+    })
 
     const toggleDrawer = () => {
       drawer.value = !drawer.value
     }
 
+    onBeforeMount(() => {
+      $firebase.auth().onAuthStateChanged((user) => {
+        if (user !== null) {
+          account.displayName = user.displayName || ''
+          account.email = user.email || ''
+          account.photoUrl = user.photoURL || ''
+        }
+      })
+    })
+
     return {
       drawer,
+      account,
       toggleDrawer,
     }
   },
