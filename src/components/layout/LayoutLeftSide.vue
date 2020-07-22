28b1999199128816270e723b40a40132e8f358e6
ozelotjp <contact@ozelot.app>
2020-07-22 09:37:40 +0900

    chore: DrawerListのテンプレート機能を削除

--- src/components/layout/LayoutLeftSide.vue
+++ src/components/layout/LayoutLeftSide.vue
@@ -3,62 +3,30 @@
     <v-container class="text-center d-lg-none">
       NuxtTS-Template
     </v-container>
-    <div v-for="(list, listIndex) in drawerList" :key="`list-${listIndex}`">
-      <v-divider v-if="listIndex !== 0" />
-      <v-list dense nav :subheader="typeof list.subheader !== 'undefined'">
-        <v-subheader v-if="typeof list.subheader !== 'undefined'">
-          {{ list.subheader }}
-        </v-subheader>
-        <v-list-item-group color="primary">
-          <v-list-item
-            v-for="(item, itemIndex) in list.items"
-            :key="`list-${itemIndex}`"
-            :to="item.to"
-          >
-            <v-list-item-icon v-if="typeof item.icon !== 'undefined'">
-              <v-icon>
-                {{ item.icon }}
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-avatar v-if="typeof item.img !== 'undefined'">
-              <v-img :src="item.img" />
-            </v-list-item-avatar>
-            <v-list-item-content>
-              {{ item.text }}
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </div>
+    <v-list dense nav>
+      <v-list-item-group color="primary">
+        <v-list-item to="/">
+          <v-list-item-icon>
+            <v-icon>
+              mdi-home
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            ホーム
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref } from 'nuxt-composition-api'
 
-interface IDrawerList {
-  subheader?: string
-  items: {
-    text: string
-    to: string
-    icon?: string
-    img?: string
-  }[]
-}
-
 export default defineComponent({
   setup(_, { root: { $vuetify } }) {
     const drawer = ref(null as boolean | null)
     const isScreenXs = computed(() => $vuetify.breakpoint.xs)
-
-    const drawerList = [
-      {
-        items: [
-          { text: 'ホーム', to: '/', icon: 'mdi-home' },
-          { text: 'ホーム', to: '/hogehoge', icon: 'mdi-home' },
-        ],
-      },
-    ] as IDrawerList[]
 
     const toggleDrawer = () => {
       drawer.value = !drawer.value
@@ -67,7 +35,6 @@ export default defineComponent({
     return {
       drawer,
       isScreenXs,
-      drawerList,
       toggleDrawer,
     }
   },
